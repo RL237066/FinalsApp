69544de7b7210daf93d9a92406b8aa634c98cd20
Kevin <RL237066@stu.rlas-116.org>
2024-12-11 08:53:01 -0600

    Initial Commit

---
+++ FinalsApp/FinalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalsAppApp.swift
+//  FinalsApp
+//
+//  Created by KEVIN DUMITRESCU on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
